19b824a5975129be3a1b383ff25b26762ba4c01d
virtual-frame-7789 <mttwright@gmail.com>
2019-10-17 00:24:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if @a == @b && @b == @c
       :equilateral
-    elsif @a == @b || @b == @c
+    elsif @a == @b || @b == @c || @a == @c
       :isosceles
     end
   end
