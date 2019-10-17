443d7e323891d5aee32705d0a4913086d4993ce3
virtual-frame-7789 <mttwright@gmail.com>
2019-10-17 00:55:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    
+
+  end
+  
+  def kind
+    if @a <= 0 && @b <= 0 && @c <= 0
+      raise TriangleError
+    elsif @a < 0 || @b < 0 || @c < 0
+      raise TriangleError
+    elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+      raise TriangleError
+    end
+    
+    
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+    
+
+  end
+    
+    
+  
+  
+  class TriangleError < StandardError
+  end
+  
+  
 end
