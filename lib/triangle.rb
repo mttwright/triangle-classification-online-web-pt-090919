6bf197b74fde1b2cc9b7051908a7eadce044b645
virtual-frame-7789 <mttwright@gmail.com>
2019-10-17 00:55:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,8 @@ class Triangle
       raise TriangleError
     elsif @a < 0 || @b < 0 || @c < 0
       raise TriangleError
+    elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+      raise TriangleError
     end
     
     
